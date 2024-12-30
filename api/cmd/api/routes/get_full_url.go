b5132a5b323cfebd2a39f12eb6e315e2a665241f
André Felipe <academicworks.adf@gmail.com>
2024-12-30 15:44:23 -0300

    adding redirects and creating 404 page

--- api/cmd/api/routes/get_full_url.go
+++ api/cmd/api/routes/get_full_url.go
@@ -2,18 +2,33 @@ package routes
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
+	"strings"
 
 	"github.com/andresilvase/cutlink/cmd/api/utils"
+	"github.com/andresilvase/cutlink/internal"
 	"github.com/andresilvase/cutlink/internal/controller"
 	apperrors "github.com/andresilvase/cutlink/internal/errors"
 	"github.com/andresilvase/cutlink/internal/models"
 	"github.com/go-chi/chi/v5"
 )
 
+func getFullURL(r *http.Request) string {
+	scheme := "http"
+
+	if r.TLS != nil {
+		scheme = "https"
+	}
+	return fmt.Sprintf("%s://%s%s\n", scheme, r.Host, r.RequestURI)
+}
+
 func FullURL(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
+
+	pathRequest := getFullURL(r)
+	isAPIEndpoint := strings.Contains(pathRequest, "api")
 
 	shortenedUrlParameter := chi.URLParam(r, "shortenedUrl")
 
@@ -28,11 +43,22 @@ func FullURL(w http.ResponseWriter, r *http.Request) {
 			statusCode = http.StatusNotFound
 		}
 
-		utils.SendResponse(
-			w,
-			utils.Response{Error: msgError},
-			statusCode,
-		)
+		if isAPIEndpoint {
+			utils.SendResponse(
+				w,
+				utils.Response{Error: msgError},
+				statusCode,
+			)
+
+		} else {
+			http.Redirect(
+				w,
+				r,
+				fmt.Sprintf("%s%s", internal.BASE_URL, "/not-found/"),
+				http.StatusFound,
+			)
+		}
+
 		return
 	}
 
@@ -40,10 +66,22 @@ func FullURL(w http.ResponseWriter, r *http.Request) {
 		URL: fullURL,
 	}
 
-	http.Redirect(
-		w,
-		r,
-		response.URL,
-		http.StatusFound,
-	)
+	if isAPIEndpoint {
+		utils.SendResponse(
+			w,
+			utils.Response{
+				Data: response,
+			},
+			http.StatusOK,
+		)
+	} else {
+		http.Redirect(
+			w,
+			r,
+			response.URL,
+			http.StatusFound,
+		)
+	}
+
+	return
 }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,33 +10,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'CutLink'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? getQueryParameter(String name) {
+    return Uri.base.path;
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launchUrl(url, webOnlyWindowName: "_self")) {
+      throw Exception('Could not launch $url');
+    }
   }
 
   @override
@@ -43,27 +40,8 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: Center(),
     );
   }
 }
