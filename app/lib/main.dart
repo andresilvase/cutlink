f5e1bb094c73828c2ddecaaee742fe724ca85b5c
André Felipe <academicworks.adf@gmail.com>
2024-12-31 13:38:46 -0300

    adding firebase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,15 +1,33 @@
-import 'package:flutter/foundation.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cutlink/core/init_dependencies.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cutlink/core/routes/router.dart';
 import 'package:cutlink/core/routes/routes.dart';
+import 'package:cutlink/firebase_options.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+void countAccess() {
+  if (!kDebugMode) {
+    FirebaseFirestore.instance
+        .doc("${kDebugMode ? "debug" : "prod"}/access")
+        .set({"count": FieldValue.increment(1)}, SetOptions(merge: true));
+  }
+}
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  countAccess();
 
   if (!kIsWeb) {
     MobileAds.instance.initialize();
