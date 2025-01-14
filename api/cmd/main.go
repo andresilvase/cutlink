ac0f7ab05fc50dde7d53e88197f592b81799d16e
André Felipe <academicworks.adf@gmail.com>
2025-01-14 09:22:05 -0300

    remove unused API files and Docker configuration

--- api/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/andresilvase/cutlink/cmd/api"
-)
-
-func main() {
-	api.Run()
-}
--- api/internal/models/models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type ShortenedURL struct {
-	ShortenedURL string `json:"shortened_url"`
-}
-
-type FullURL struct {
-	URL string `json:"url"`
-}
